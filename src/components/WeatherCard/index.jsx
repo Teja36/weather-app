c72ee3563e37f435911058978377937af8e902c3
Teja36 <bommarajutej@gmail.com>
2022-08-14 09:52:10 +0530

    updated style of weather-card

--- src/components/WeatherCard/index.jsx
+++ src/components/WeatherCard/index.jsx
@@ -6,19 +6,21 @@ const WeatherCard = ({ details }) => {
   else
     return (
       <div className={styles.container}>
-        <h1 className={styles.h1}>
-          {details?.city},{" "}
-          {details.country === "United States of America"
-            ? "USA"
-            : details.country}
-        </h1>
-        <p className={styles.p1}>{details.dateInWords}</p>
-        <div className={styles.tempContainer}>
-          <img
-            className={styles.img}
-            src={details.currentCondition.icon}
-            alt="weather-icon"
-          />
+        <div className={styles.city}>
+          <h1 className={styles.h1}>
+            {details?.city},{" "}
+            {details.country === "United States of America"
+              ? "USA"
+              : details.country}
+          </h1>
+          <p className={styles.p1}>{details.dateInWords}</p>
+        </div>
+        <img
+          className={styles.img}
+          src={details.currentCondition.icon}
+          alt="weather-icon"
+        />
+        <div className={styles.temp}>
           <h2 className={styles.h2}>{details.currentTemp}&deg;c</h2>
           <p className={styles.p}>{details.currentCondition?.text}</p>
         </div>
